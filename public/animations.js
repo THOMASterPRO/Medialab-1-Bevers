2439c486ab199ede8df01de5b6644c3514d8decf
athomaster <thomas@bezemer.net>
2023-03-28 00:17:34 +0200

    added first animation

---
+++ public/animations.js
@@ -0,0 +1,12 @@
+// document.getElementById("knop").addEventListener("click", startAnimation);
+
+// function startAnimation() {
+//     document.getElementById("laadwiel").style.display;
+// }
+
+document.getElementById("knop").addEventListener("click", startAnimation);
+
+function startAnimation() {
+    document.getElementById("laadwiel").classList.remove('invisible');
+
+}
